27f9d9bd0361060668fad852c4cef156edf3549e
radhikarajkumar <48429791+radhikarajkumar@users.noreply.github.com>
2019-03-26 15:41:28 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,1 @@
-Invoke-Sqlcmd -InputFile "C://Users/radhika.rajkumar/Desktop/test/test.sql" -ServerInstance "CHNMCT399683D" -database "master"
+Invoke-Sqlcmd -InputFile "https://github.com/radhikarajkumar/repo_2/blob/master/test_script1.sql" -ServerInstance "CHNMCT399683D" -database "master"
